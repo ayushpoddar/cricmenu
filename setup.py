094b6ad3fb31b85da641d500f1c0dc3d2acdc34e
Ayush Poddar <ayush.mail.id@gmail.com>
2021-03-03 14:11:05 +0530

    change python version

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+APP = ['live.py']
+DATA_FILES = ["notification.py"]
+OPTIONS = {
+    'argv_emulation': True,
+    'iconfile': 'icon.icns',
+    'plist': {
+        'CFBundleShortVersionString': '1.0.0',
+        'LSUIElement': True,
+    },
+    'packages': ['rumps', 'requests'],
+}
+
+setup(
+    app=APP,
+    name='CricMenu',
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'], 
+    install_requires=['rumps', 'requests']
+)
